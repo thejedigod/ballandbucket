c960ff4d312aa8d2cb12219711ffdf17d8ee8e3e
thejedigod <74409484+thejedigod@users.noreply.github.com>
2021-02-06 14:56:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ log3.display();
 
 function keyPressed(){
 	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(paper.body,paper.body.position,{x: 3,y:-3});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x: 4,y:-4});
 	}
 }
 
